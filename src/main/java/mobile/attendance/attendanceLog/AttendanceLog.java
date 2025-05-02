a70e000195b5600eaea28326278101ae36ea36c6
pingmong <kny7024@naver.com>
2025-05-02 16:37:15 +0900

    AttendanceLog 기본 CRUD

--- src/main/java/mobile/attendance/attendanceLog/AttendanceLog.java
+++ src/main/java/mobile/attendance/attendanceLog/AttendanceLog.java
@@ -1,4 +1,87 @@
 package mobile.attendance.attendanceLog;
 
+import java.sql.Timestamp;
+
 public class AttendanceLog {
+    private Long logId;
+    private String userId;
+    private int attendanceId;
+    private Timestamp checkInAt;
+    private Timestamp checkOutAt;
+    private boolean isPresent;
+    private String note;
+
+    public AttendanceLog() {}
+
+    public AttendanceLog(final Long logId, final String userId, final int attendanceId, final Timestamp checkInAt, final Timestamp checkOutAt, final boolean isPresent, final String note) {
+        this.logId = logId;
+        this.userId = userId;
+        this.attendanceId = attendanceId;
+        this.checkInAt = checkInAt;
+        this.checkOutAt = checkOutAt;
+        this.isPresent = isPresent;
+        this.note = note;
+    }
+
+    public AttendanceLog(final String userId, final int attendanceId) {
+    }
+
+    public AttendanceLog(final Long logId, final String note) {
+    }
+
+    public Long getLogId() {
+        return logId;
+    }
+
+    public void setLogId(final Long logId) {
+        this.logId = logId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(final String userId) {
+        this.userId = userId;
+    }
+
+    public int getAttendanceId() {
+        return attendanceId;
+    }
+
+    public void setAttendanceId(final int attendanceId) {
+        this.attendanceId = attendanceId;
+    }
+
+    public Timestamp getCheckInAt() {
+        return checkInAt;
+    }
+
+    public void setCheckInAt(final Timestamp checkInAt) {
+        this.checkInAt = checkInAt;
+    }
+
+    public Timestamp getCheckOutAt() {
+        return checkOutAt;
+    }
+
+    public void setCheckOutAt(final Timestamp checkOutAt) {
+        this.checkOutAt = checkOutAt;
+    }
+
+    public boolean isPresent() {
+        return isPresent;
+    }
+
+    public void setPresent(final boolean present) {
+        isPresent = present;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(final String note) {
+        this.note = note;
+    }
 }
--- src/main/java/mobile/attendance/attendanceLog/AttendanceLogController.java
+++ src/main/java/mobile/attendance/attendanceLog/AttendanceLogController.java
@@ -1,4 +1,50 @@
 package mobile.attendance.attendanceLog;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public class AttendanceLogController {
+
+    AttendanceLogService attendanceLogService;
+
+    public AttendanceLogController(AttendanceLogService attendanceLogService) {
+        this.attendanceLogService = attendanceLogService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AttendanceLog> createAttendanceLog(@RequestBody final AttendanceLogRequest request) {
+        AttendanceLog attendanceLog = new AttendanceLog(
+                request.getUserId(), request.getAttendanceId());
+
+        return ResponseEntity.ok(attendanceLogService.createAttendanceLog(attendanceLog));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AttendanceLog>> getAttendances() {
+        return ResponseEntity.ok(attendanceLogService.findAllAttendanceLogs());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AttendanceLog> getAttendance(@RequestBody final Long id) {
+        Optional<AttendanceLog> attendanceLog = attendanceLogService.findById(id);
+        return attendanceLog.isPresent() ? ResponseEntity.ok(attendanceLog.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<String> updateAttendance(@PathVariable("id") final Long logId,
+                                                   @RequestBody final AttendanceLogRequest request) {
+        AttendanceLog updateAttendance = new AttendanceLog(logId, request.getNote());
+        attendanceLogService.updateAttendanceLog(updateAttendance);
+        return ResponseEntity.ok("성공적으로 기록 노트가 수정되었습니다.");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> removeAttendance(@PathVariable("id") final Long id) {
+        attendanceLogService.removeAttendanceLog(id);
+        return ResponseEntity.ok("성공적으로 기록 노트가 삭제되었습니다.");
+    }
 }
