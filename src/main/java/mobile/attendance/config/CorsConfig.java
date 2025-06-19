f9273e5a2a4db245abefc3073f13a666e605dc06
pingmong <kny7024@naver.com>
2025-06-20 08:40:40 +0900

    feat: cors 경로 추가

--- src/main/java/mobile/attendance/config/CorsConfig.java
+++ src/main/java/mobile/attendance/config/CorsConfig.java
@@ -29,7 +29,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                                "http://43.203.120.210:8080"
+                )
+
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
